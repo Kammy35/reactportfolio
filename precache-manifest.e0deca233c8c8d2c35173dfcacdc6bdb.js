09ba439dab24d3954c4a9f88100a7e32e53b7b37
codenation <codenation@codenations-MacBook-Pro-2.local>
2019-10-02 16:18:33 +0100

    Updates

---
+++ precache-manifest.e0deca233c8c8d2c35173dfcacdc6bdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a36adffdafddbc7612ddbba52e270f9",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "cd9691398d26f3b91cbc",
+    "url": "/reactportfolio/static/css/main.07649cea.chunk.css"
+  },
+  {
+    "revision": "6f6c0f29478d92308e66",
+    "url": "/reactportfolio/static/js/2.c6481815.chunk.js"
+  },
+  {
+    "revision": "cd9691398d26f3b91cbc",
+    "url": "/reactportfolio/static/js/main.eaeb1c19.chunk.js"
+  },
+  {
+    "revision": "8007ccf6b0ab45eb369a",
+    "url": "/reactportfolio/static/js/runtime~main.d7794af4.js"
+  },
+  {
+    "revision": "549c4aef6bdd358e107162ded15228b7",
+    "url": "/reactportfolio/static/media/kammy1.549c4aef.jpg"
+  }
+]);
